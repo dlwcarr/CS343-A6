c5304c85d39fa677161351cb2cd40db36c40511f
Dane Carr <dane.l.w.carr@gmail.com>
2013-11-30 17:28:39 -0500

    started VendingMachine

--- vendingMachine.h
+++ vendingMachine.h
@@ -1,7 +1,21 @@
+#include <uC++.h>
+#include "printer.h"
+#include "nameServer.h"
+#include "watcard.h"
+#include <vector>
+
 _Task VendingMachine {
     void main();
+
+    Printer& printer;
+    NamerServer& nameServer;
+    unsigned int id, sodaCost, maxStockPerFlavour;
+    const unsigned int NUM_FLAVOURS = 4;    // keep up to date after changing Flavours enum
+    unsigned int* stock;
+    bool stocking;
+
   public:
-    enum Flavours { ... };                 // flavours of soda (YOU DEFINE)
+    enum Flavours { BLACK_CHERRY, CREAM_SODA, ROOT_BEER, LIME };                 // flavours of soda (YOU DEFINE)
     enum Status { BUY, STOCK, FUNDS };     // purchase status: successful buy, out of stock, insufficient funds
     VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
                     unsigned int maxStockPerFlavour );
