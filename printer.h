abc25146f42181761ce7daed0beba387dedc69c0
AlexLaviolette <alex.laviolette92@gmail.com>
2013-11-30 22:53:55 -0500

    Printer done

--- printer.h
+++ printer.h
@@ -1,4 +1,13 @@
 _Monitor Printer {
+	unsigned int numStudents;
+	unsigned int numVendingMachines;
+	unsigned int numCouriers;
+	unsigned int total;
+    char* buff;                                 // stores current state
+    int* val1buff;                              // stores number associated with current state
+    int* val2buff;
+    void flush();                               // flushes and prints buffer
+    unsigned int getPos( Kind kind, unsigned int lid );	// returns position in buffer
   public:
     enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
     Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
@@ -8,4 +17,6 @@ _Monitor Printer {
     void print( Kind kind, unsigned int lid, char state );
     void print( Kind kind, unsigned int lid, char state, int value1 );
     void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+
+    virtual ~Printer();
 };
