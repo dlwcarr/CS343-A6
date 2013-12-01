8bf0e9c0526af24e0f2b3ea6cd3637e6f77d2428
AlexLaviolette <alex.laviolette92@gmail.com>
2013-11-30 23:21:31 -0500

    Bank done

--- bank.cc
+++ bank.cc
@@ -1,13 +1,28 @@
 #include "bank.h"
 
-Bank::Bank( unsigned int numStudents ) {
+Bank::Bank( unsigned int numStudents ) : numStudents(numStudents) {
+	accounts = new char[numStudents]();
 
+	for ( unsigned int i = 0; i < numStudents; i++ ) {
+		conds.push_back(new uCondition);
+	}
 }
 
 void Bank::deposit( unsigned int id, unsigned int amount ) {
-
+	accounts[id] += amount;
+	conds[id]->signal();
 }
 
 void Bank::withdraw( unsigned int id, unsigned int amount ) {
-	
+	while ( accounts[id] < amount ) {
+		conds[id]->wait();
+	}
+
+	accounts[id] -= amount;
+
+}
+
+Bank::~Bank() {
+	delete[] accounts;
+	while(!conds.empty()) delete conds.back(), conds.pop_back();
 }
--- bank.h
+++ bank.h
@@ -1,6 +1,13 @@
+#include <vector>
+
 _Monitor Bank {
+	unsigned int numStudents;
+	int* accounts;
+	vector<uCondition*> conds;
   public:
     Bank( unsigned int numStudents );
     void deposit( unsigned int id, unsigned int amount );
     void withdraw( unsigned int id, unsigned int amount );
+
+    virtual ~Bank();
 };
