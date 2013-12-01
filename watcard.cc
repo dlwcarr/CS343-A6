bd9f1b07347072375346198979f30331cc02de52
AlexLaviolette <alex.laviolette92@gmail.com>
2013-12-01 16:06:20 -0500

    watcard stuff

--- watcard.cc
+++ watcard.cc
@@ -1,25 +1,19 @@
 #include "watcard.h"
 
-WATCard::WATCard( const WATCard & ) { // copy constructor
-
-}
-
-WATCard & WATCard::operator=( const WATCard & ) {
-
-}
-
 WATCard::WATCard() {
-
+	balance = 0;
 }
 
 void WATCard::deposit( unsigned int amount ) {
-
+	balance += amount;
+	withdraw.signal();
 }
 
 void WATCard::withdraw( unsigned int amount ) {
-
+	while ( amount < balance ) withdraw.wait();
+	balance -= amount;
 }
 
 unsigned int WATCard::getBalance() {
-
+	return balance;
 }
--- watcard.h
+++ watcard.h
@@ -1,6 +1,15 @@
+#ifndef WATCARD_H
+#define WATCARD_H
+
+#include <uFuture.h>
+
 class WATCard {
     WATCard( const WATCard & );            // prevent copying
     WATCard &operator=( const WATCard & );
+
+    unsigned int balance;
+    uCondition withdraw;
+
   public:
     WATCard();
     typedef Future_ISM<WATCard *> FWATCard; // future watcard pointer
@@ -8,3 +17,5 @@ class WATCard {
     void withdraw( unsigned int amount );
     unsigned int getBalance();
 };
+
+#endif
