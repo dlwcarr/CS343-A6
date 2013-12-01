ad34d4705cb9461b0d7681ca19a762bfff2ed2f7
Dane Carr <dane.l.w.carr@gmail.com>
2013-11-30 23:16:43 -0500

    added printing to BottlingPlant

--- bottlingPlant.cc
+++ bottlingPlant.cc
@@ -24,15 +24,23 @@ bool BottlingPlant::getShipment( unsigned int cargo[] ) {
 	for(unsigned int i = 0; i < NUM_FLAVOURS; i++)
 		cargo[i] = shipment[i];
 
+	printer.print(Printer::BottlingPlant, 'P');
+
 	return false;
 }
 
 void BottlingPlant::produceShipment() {
-	for (unsigned int i = 0; i < NUM_FLAVOURS; i++)
+	int shipmentSize = 0;
+	for (unsigned int i = 0; i < NUM_FLAVOURS; i++) {
 		shipment[id] = rng(maxShippedPerFlavour);
+		shipmentSize += shipment[id];
+	}
+	printer.print(Printer::BottlingPlant, 'G', shipmentSize);
 }
 
 void BottlingPlant::main() {
+	printer.print(Printer::BottlingPlant, 'S');
+
 	truck = new Truck(printer, nameServer, *this, numVendingMachines, maxStockPerFlavour);
 
 	yield(timeBetweenShipments);
@@ -47,4 +55,6 @@ void BottlingPlant::main() {
 			produceShipment();
 		}
 	}
+
+	printer.print(Printer::BottlingPlant, 'F');
 }
