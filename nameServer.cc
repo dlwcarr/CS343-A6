23b2623aa8bf69429af73f68801fef411511a87e
Dane Carr <dane.l.w.carr@gmail.com>
2013-12-02 15:38:18 -0500

    fix array bound

--- nameServer.cc
+++ nameServer.cc
@@ -5,7 +5,7 @@
 using namespace std;
 
 NameServer::NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents ) : printer(prt), 
-			numVendingMachines(numVendingMachines), numStudents(numStudents), lastMachine(0), machineNotRegistered(numStudents) {
+			numVendingMachines(numVendingMachines), numStudents(numStudents), lastMachine(0), machineNotRegistered(numVendingMachines) {
 	machineList = new VendingMachine* [numVendingMachines];
 	studentsMachine = new unsigned int [numStudents];
 	
